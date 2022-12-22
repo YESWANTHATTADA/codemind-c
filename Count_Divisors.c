515d7c38cf89daa6355f1d3e55fdeae23991209c
YESWANTHATTADA <119840544+YESWANTHATTADA@users.noreply.github.com>
2022-12-22 23:09:08 +0530

    #include<stdio.h> int main() {     int l,r,k;     scanf("%d%d%d",&l,&r,&k);     int c=0;     int i;     for(i=l;i<=r;i++)     {         if(i%k==0)         {             c++;         }     }     printf("%d",c); }

---
+++ Count_Divisors.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int l,r,k;
+    scanf("%d%d%d",&l,&r,&k);
+    int c=0;
+    int i;
+    for(i=l;i<=r;i++)
+    {
+        if(i%k==0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
