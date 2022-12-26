9d0fa04d209f9c0c6eba8dd9d91d2b23fffc93d1
YESWANTHATTADA <119840544+YESWANTHATTADA@users.noreply.github.com>
2022-12-26 21:55:46 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int a[n],i,m;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&m);     int f=0;     for(i=0;i<n;i++)     {         f=f+a[i];
        }
        int g;
        float y;
        y=f/n;
        for(i=0;i<n;i++)
        {
            if(y==a[i])
            {
                g=1;
            }
        }
        if(g==1)
        {
            printf("True");
        }
        else
        {
            printf("False");
        }
    }

---
+++ average_element_is_in_array_or_not.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n],i,m;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&m);
+    int f=0;
+    for(i=0;i<n;i++)
+    {
+        f=f+a[i];
+        
+    }
+    int g;
+    float y;
+    y=f/n;
+    for(i=0;i<n;i++)
+    {
+        if(y==a[i])
+        {
+            g=1;
+        }
+    }
+    if(g==1)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
