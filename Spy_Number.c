de6f4b061eeb5610cb22e3ee69c962269b3639a8
YESWANTHATTADA <119840544+YESWANTHATTADA@users.noreply.github.com>
2023-01-02 20:50:34 +0530

    #include<stdio.h> int main() {
        int n,s=0,p=1,r;
        scanf("%d",&n);
        while(n!=0)
        {
            r=n%10;
            s=s+r;
            p=p*r;
            n=n/10;
        }
        if(s==p)
        {
            printf("Spy Number");
        }
        else
        {
            printf("Not Spy Number");
        }
    }

--- Spy_Number.c
+++ Spy_Number.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
-const char*spy(int num)
-{
-    int r,s=0,p=1;
-    while(num!=0)
-    {
-        r=num%10;
-        s=s+r;
-        p=p*r;
-        num=num/10;
-    }
-    if(s==p)
-    return("Spy Number");
-    else
-    return("Not Spy Number");
-}
 int main()
 {
-    int num;
-    scanf("%d",&num);
-    printf("%s",spy(num));
+    
+    int n,s=0,p=1,r;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        r=n%10;
+        s=s+r;
+        p=p*r;
+        n=n/10;
+    }
+    if(s==p)
+    {
+        printf("Spy Number");
+    }
+    else
+    {
+        printf("Not Spy Number");
+    }
 }
