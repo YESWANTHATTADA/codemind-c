d943fe99ff0fdcd47c07bf53003d4d5b8fdb9690
YESWANTHATTADA <119840544+YESWANTHATTADA@users.noreply.github.com>
2023-01-06 20:21:21 +0530

    #include<stdio.h> int main(){     int n;     scanf("%d",&n);     int a[n],i,m;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     m=0;     for(i=0;i<n;i++)     {         m=m+a[i];
        }
        
        float y;
        y=float (m)/n;
        printf("%.2f",y);
    }

--- average_of_the_array.c
+++ average_of_the_array.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
-int main()
-{
-    int n,m,i;
+int main(){
+    int n;
     scanf("%d",&n);
-    int a[n];
+    int a[n],i,m;
     for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
@@ -12,8 +11,10 @@ int main()
     for(i=0;i<n;i++)
     {
         m=m+a[i];
+        
     }
+    
     float y;
-    y=float(m)/n;
+    y=float (m)/n;
     printf("%.2f",y);
 }
