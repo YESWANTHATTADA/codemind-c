89584cc5435c40ab9a4a410cfaa4b32bdbe33d01
YESWANTHATTADA <119840544+YESWANTHATTADA@users.noreply.github.com>
2023-01-20 21:26:52 +0530

    #include<stdio.h> int main() {     int i,n,a;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         a=i*3;         printf("%d ",a);     } }

--- first_N_multiples_of_3.c
+++ first_N_multiples_of_3.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int i,n;
+    int i,n,a;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        int a=i*3;
+        a=i*3;
         printf("%d ",a);
     }
 }
